66a6e5728951153981e83e27780e60e08db554ee
matlavv <mathis.laversin92@gmail.com>
2024-02-09 17:02:29 +0100

    LeftColumn stylised

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,17 @@ const bison = localFont({ src: "../assets/fonts/bison.ttf" });
 
 export default function Home() {
   return (
-    <div className={bison.className}>
+    <div className={`${bison.className} min-h-screen flex flex-col`}>
       <Header />
-      <div className="flex justify-between p-5">
+      <div className="flex flex-grow justify-between">
         {/* Left side */}
-        <div className="w-1/2">
+        <div className="w-1/2 bg-myPink h-100">
           <LeftColumn />
         </div>
+        {/* Vertical bar */}
+        <div className="w-0.5 bg-black self-stretch"></div>
         {/* Right side */}
-        <div className="w-1/2">
+        <div className="w-1/2 mt-4">
           <RightColumn />
         </div>
       </div>
--- components/RightColumn/RightColumn.tsx
+++ components/RightColumn/RightColumn.tsx
@@ -1,13 +1,13 @@
-import GifBar from "../LeftColumn/GifBar";
-import Quote from "../LeftColumn/Quote";
+import GifBar from "./GifBar";
+import Quote from "./Quote";
 
 const RightColumn = () => {
   return (
     <div className="flex-col">
-      <div className=" ml-8 w-5/6 ">
+      <div className="mt-5 ml-8 w-5/6 ">
         <Quote />
       </div>
-      <div className="mt-6 ml-8 w-5/6">
+      <div className="mt-5 ml-8 w-5/6">
         <GifBar />
       </div>
     </div>
