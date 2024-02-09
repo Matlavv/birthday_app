ba93a08d8c2b2329d42f0e3d9d766bf0bdad2ed6
matlavv <mathis.laversin92@gmail.com>
2024-02-09 14:31:10 +0100

    Progressive bar

--- components/RightColumn/RightColumn.tsx
+++ components/RightColumn/RightColumn.tsx
@@ -4,10 +4,10 @@ import Quote from "../LeftColumn/Quote";
 const RightColumn = () => {
   return (
     <div className="flex-col">
-      <div className="mt-4 ml-8 w-5/6 ">
+      <div className=" ml-8 w-5/6 ">
         <Quote />
       </div>
-      <div className="mt-8 ml-8 w-5/6">
+      <div className="mt-6 ml-8 w-5/6">
         <GifBar />
       </div>
     </div>
