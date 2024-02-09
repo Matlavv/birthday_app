3b7d79eaa3c41983d8dab7ee36c57cd4c1f36c66
matlavv <mathis.laversin92@gmail.com>
2024-02-09 14:33:48 +0100

    clean code

--- components/LeftColumn/Quote.tsx
+++ components/LeftColumn/Quote.tsx
@@ -1,7 +1,7 @@
 const Quote = () => {
   return (
     <div className="flex-row p-4 border border-black rounded-xl shadow-md shadow-myPink">
-      <p className="">
+      <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
         quam commodi rem minus deleniti quia excepturi animi nobis aliquam aut
         quaerat voluptates delectus eveniet iusto voluptatibus, expedita
